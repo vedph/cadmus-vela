95e031b89e0686f77ed1e22e4a99617eebe8b02f
Myrmex <dfusi@hotmail.com>
2023-03-21 14:58:40 +0100

    Services

---
+++ Cadmus.Vela.Services/VelaPartSeederFactoryProvider.cs
@@ -0,0 +1,61 @@
+﻿using Cadmus.Core.Config;
+using Cadmus.Seed;
+using Cadmus.Seed.Epigraphy.Parts;
+using Cadmus.Seed.General.Parts;
+using Cadmus.Seed.Philology.Parts;
+using Cadmus.Seed.Vela.Parts;
+using Fusi.Microsoft.Extensions.Configuration.InMemoryJson;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Reflection;
+
+namespace Cadmus.Vela.Services;
+
+/// <summary>
+/// VeLA part seeders provider.
+/// </summary>
+/// <seealso cref="IPartSeederFactoryProvider" />
+public sealed class VelaPartSeederFactoryProvider : IPartSeederFactoryProvider
+{
+    private static IHost GetHost(string config)
+    {
+        // build the tags to types map for parts/fragments
+        Assembly[] seedAssemblies = new[]
+        {
+            // Cadmus.Seed.General.Parts
+            typeof(NotePartSeeder).Assembly,
+            // Cadmus.Seed.Philology.Parts
+            typeof(ApparatusLayerFragmentSeeder).Assembly,
+            // Cadmus.Seed.Epigraphy.Parts
+            typeof(EpiLigaturesLayerFragmentSeeder).Assembly,
+            // Cadmus.Seed.Vela.Parts
+            typeof(GrfSummaryPartSeeder).GetTypeInfo().Assembly,
+        };
+        TagAttributeToTypeMap map = new();
+        map.Add(seedAssemblies);
+
+        return new HostBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                PartSeederFactory.ConfigureServices(services,
+                    new StandardPartTypeProvider(map),
+                    seedAssemblies);
+            })
+            // extension method from Fusi library
+            .AddInMemoryJson(config)
+            .Build();
+    }
+
+    /// <summary>
+    /// Gets the part/fragment seeders factory.
+    /// </summary>
+    /// <param name="profile">The profile.</param>
+    /// <returns>Factory.</returns>
+    /// <exception cref="ArgumentNullException">profile</exception>
+    public PartSeederFactory GetFactory(string profile)
+    {
+        if (profile == null) throw new ArgumentNullException(nameof(profile));
+
+        return new PartSeederFactory(GetHost(profile));
+    }
+}
---
+++ Cadmus.Vela.Services/VelaRepositoryProvider.cs
@@ -0,0 +1,78 @@
+﻿using System.Reflection;
+using Cadmus.Core;
+using Cadmus.Core.Config;
+using Cadmus.Core.Storage;
+using Cadmus.Mongo;
+using Cadmus.General.Parts;
+using Cadmus.Philology.Parts;
+using System;
+using Cadmus.Vela.Parts;
+using Cadmus.Epigraphy.Parts;
+
+namespace Cadmus.Vela.Services;
+
+/// <summary>
+/// Cadmus __PRJ__ repository provider.
+/// </summary>
+/// <seealso cref="IRepositoryProvider" />
+public sealed class VelaRepositoryProvider
+{
+    private readonly IPartTypeProvider _partTypeProvider;
+
+    /// <summary>
+    /// The connection string.
+    /// </summary>
+    public string ConnectionString { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="__PRJ__RepositoryProvider"/>
+    /// class.
+    /// </summary>
+    public VelaRepositoryProvider()
+    {
+        ConnectionString = "";
+        TagAttributeToTypeMap map = new();
+        map.Add(new[]
+        {
+            // Cadmus.General.Parts
+            typeof(NotePart).GetTypeInfo().Assembly,
+            // Cadmus.Philology.Parts
+            typeof(ApparatusLayerFragment).GetTypeInfo().Assembly,
+            // Cadmus.Epigraphy.Parts
+            typeof(EpiSupportPart).GetTypeInfo().Assembly,
+            // Cadmus.Vela.Parts
+            typeof(GrfSummaryPart).GetTypeInfo().Assembly,
+        });
+
+        _partTypeProvider = new StandardPartTypeProvider(map);
+    }
+
+    /// <summary>
+    /// Gets the part type provider.
+    /// </summary>
+    /// <returns>part type provider</returns>
+    public IPartTypeProvider GetPartTypeProvider()
+    {
+        return _partTypeProvider;
+    }
+
+    /// <summary>
+    /// Creates a Cadmus repository.
+    /// </summary>
+    /// <returns>repository</returns>
+    public ICadmusRepository CreateRepository()
+    {
+        // create the repository (no need to use container here)
+        MongoCadmusRepository repository = new(_partTypeProvider,
+                new StandardItemSortKeyBuilder());
+
+        repository.Configure(new MongoCadmusRepositoryOptions
+        {
+            ConnectionString = ConnectionString ??
+            throw new InvalidOperationException(
+                "No connection string set for IRepositoryProvider implementation")
+        });
+
+        return repository;
+    }
+}
