2f487f35c954bec4a6da48c009aaf28456a785eb
Myrmex <dfusi@hotmail.com>
2023-03-21 14:30:00 +0100

    GrfWritingPartSeeder

---
+++ Cadmus.Seed.Vela.Parts/GrfWritingPartSeeder.cs
@@ -0,0 +1,68 @@
+﻿using Bogus;
+using Cadmus.Core;
+using Cadmus.Refs.Bricks;
+using Cadmus.Vela.Parts;
+using Fusi.Tools.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace Cadmus.Seed.Vela.Parts;
+
+/// <summary>
+/// Seeder for <see cref="GrfWritingPart"/>.
+/// Tag: <c>seed.it.vedph.graffiti.writing</c>.
+/// </summary>
+/// <seealso cref="PartSeederBase" />
+[Tag("seed.it.vedph.graffiti.writing")]
+public sealed class GrfWritingPartSeeder : PartSeederBase
+{
+    private static List<DecoratedCount> GetCounts(Faker f)
+    {
+        return new List<DecoratedCount>
+        {
+            new DecoratedCount
+            {
+                Id = "row",
+                Value = f.Random.Number(1, 50)
+            }
+        };
+    }
+
+    /// <summary>
+    /// Creates and seeds a new part.
+    /// </summary>
+    /// <param name="item">The item this part should belong to.</param>
+    /// <param name="roleId">The optional part role ID.</param>
+    /// <param name="factory">The part seeder factory. This is used
+    /// for layer parts, which need to seed a set of fragments.</param>
+    /// <returns>A new part or null.</returns>
+    /// <exception cref="ArgumentNullException">item or factory</exception>
+    public override IPart? GetPart(IItem item, string? roleId,
+        PartSeederFactory? factory)
+    {
+        if (item == null) throw new ArgumentNullException(nameof(item));
+
+        GrfWritingPart part = new Faker<GrfWritingPart>()
+            .RuleFor(p => p.System, f => f.PickRandom("latn", "grek"))
+            .RuleFor(p => p.Languages, f => new List<string>
+            {
+                f.PickRandom("lat", "grc")
+            })
+            .RuleFor(p => p.Type, f => f.PickRandom("upper", "lower"))
+            .RuleFor(p => p.Counts, f => GetCounts(f))
+            .RuleFor(p => p.Features, f => new List<string>
+            {
+                f.PickRandom("text", "digit")
+            })
+            .RuleFor(p => p.HasPoetry, f => f.Random.Bool(0.25f))
+            .RuleFor(p => p.Metres, f => new List<string>
+            {
+                f.PickRandom("-", "s7")
+            })
+            .Generate();
+
+        SetPartMetadata(part, roleId, item);
+
+        return part;
+    }
+}
