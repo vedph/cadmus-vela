6bd4bccbc3a9b605476186dc6990f2a654bbf14f
Myrmex <dfusi@hotmail.com>
2023-03-21 15:41:01 +0100

    Update GrfSummaryPartTest.cs

--- Cadmus.Vela.Parts.Test/GrfSummaryPartTest.cs
+++ Cadmus.Vela.Parts.Test/GrfSummaryPartTest.cs
@@ -4,6 +4,9 @@ using Cadmus.Core;
 using System.Collections.Generic;
 using System.Linq;
 using Cadmus.Seed.Vela.Parts;
+using Cadmus.Refs.Bricks;
+using Cadmus.Mat.Bricks;
+using Fusi.Antiquity.Chronology;
 
 namespace Cadmus.Vela.Parts.Test;
 
@@ -55,13 +58,90 @@ public sealed class GrfSummaryPartTest
     public void GetDataPins_Tag_1()
     {
         GrfSummaryPart part = GetEmptyPart();
-        // TODO: set only the properties required for pins
-        // in a predictable way so we can test them
+        part.Place = new AssertedProperName
+        {
+            Language = "ita",
+        };
+        part.Place.Pieces!.Add(new ProperNamePiece
+        {
+            Type = "sestriere",
+            Value = "Dorsoduro"
+        });
+        part.SupportType = "support";
+        part.ObjectType = "object";
+        part.OriginalFn = "original";
+        part.CurrentFn = "current";
+        part.Indoor = true;
+        part.Material = "limestone";
+        part.Size = new PhysicalSize
+        {
+            W = new PhysicalDimension
+            {
+                Value = 10,
+                Unit = "cm"
+            }
+        };
+        part.Date = HistoricalDate.Parse("1234 AD");
+        part.Features.Add("fa");
+        part.Features.Add("fb");
+        part.LastSeen = new DateTime(2020, 1, 1);
+        part.States.Add(new GrfSupportState
+        {
+            Type = "s1"
+        });
 
         List<DataPin> pins = part.GetDataPins(null).ToList();
-        Assert.Single(pins);
+        Assert.Equal(13, pins.Count);
 
-        DataPin? pin = pins.Find(p => p.Name == "id" && p.Value == "steph");
+        // place
+        DataPin? pin = pins.Find(p => p.Name == "place" && p.Value == "dorsoduro");
+        Assert.NotNull(pin);
+        TestHelper.AssertPinIds(part, pin!);
+        // support-type
+        pin = pins.Find(p => p.Name == "support-type" && p.Value == "support");
+        Assert.NotNull(pin);
+        TestHelper.AssertPinIds(part, pin!);
+        // object-type
+        pin = pins.Find(p => p.Name == "object-type" && p.Value == "object");
+        Assert.NotNull(pin);
+        TestHelper.AssertPinIds(part, pin!);
+        // original-fn
+        pin = pins.Find(p => p.Name == "original-fn" && p.Value == "original");
+        Assert.NotNull(pin);
+        TestHelper.AssertPinIds(part, pin!);
+        // current-fn
+        pin = pins.Find(p => p.Name == "current-fn" && p.Value == "current");
+        Assert.NotNull(pin);
+        TestHelper.AssertPinIds(part, pin!);
+        // indoor
+        pin = pins.Find(p => p.Name == "indoor" && p.Value == "1");
+        Assert.NotNull(pin);
+        TestHelper.AssertPinIds(part, pin!);
+        // material
+        pin = pins.Find(p => p.Name == "material" && p.Value == "limestone");
+        Assert.NotNull(pin);
+        TestHelper.AssertPinIds(part, pin!);
+        // w
+        pin = pins.Find(p => p.Name == "w" && p.Value == "10");
+        Assert.NotNull(pin);
+        TestHelper.AssertPinIds(part, pin!);
+        // date-value
+        pin = pins.Find(p => p.Name == "date-value" && p.Value == "1234");
+        Assert.NotNull(pin);
+        TestHelper.AssertPinIds(part, pin!);
+        // feature's
+        pin = pins.Find(p => p.Name == "feature" && p.Value == "fa");
+        Assert.NotNull(pin);
+        TestHelper.AssertPinIds(part, pin!);
+        pin = pins.Find(p => p.Name == "feature" && p.Value == "fb");
+        Assert.NotNull(pin);
+        TestHelper.AssertPinIds(part, pin!);
+        // yr-last-seen
+        pin = pins.Find(p => p.Name == "yr-last-seen" && p.Value == "2020");
+        Assert.NotNull(pin);
+        TestHelper.AssertPinIds(part, pin!);
+        // state
+        pin = pins.Find(p => p.Name == "state" && p.Value == "s1");
         Assert.NotNull(pin);
         TestHelper.AssertPinIds(part, pin!);
     }
